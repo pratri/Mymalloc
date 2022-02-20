b4b38f96e1533e233f15f302d0437ccc8baa226c
TheRealKlin4 <100099801+TheRealKlin4@users.noreply.github.com>
2022-02-20 18:58:55 -0500

    Update mymalloc.h

--- mymalloc.h
+++ mymalloc.h
@@ -1,1 +1,12 @@
+#ifndef MYMALLOC_H
+#define MYMALLOC_H
+#define malloc(s) mymalloc(s, __FILE__, __LINE__)
+#define free(p) myfree(p, __FILE__, __LINE__)
 
+struct ListNode;
+
+void *mymalloc(size_t size, char *file, int line);
+
+void myfree(void *ptr, char *file, int line);
+
+#endif
