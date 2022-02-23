0fee09e97f01b60bc2790820daa5f5401daf6519
TheRealKlin4 <100099801+TheRealKlin4@users.noreply.github.com>
2022-02-23 14:00:59 -0500

    Update mymalloc.c

--- mymalloc.c
+++ mymalloc.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "mymalloc.h"
 
 #define mem_size 4096
 static char memory[mem_size];
+
+#include "mymalloc.h"
 
 int main(int argc, char* argv[]){
     //Start of the linked list
