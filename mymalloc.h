4e42ddae7072330ba1516fcabcce0aca5d1c23a4
TheRealKlin4 <100099801+TheRealKlin4@users.noreply.github.com>
2022-02-23 15:07:33 -0500

    Update mymalloc.h

--- mymalloc.h
+++ mymalloc.h
@@ -13,11 +13,10 @@ typedef struct _listnode{
     int free;
     //Metadata location in memory array
     int location;
-    struct ListNode *next;
-    struct ListNode *prev;
+    struct _listnode *next;
+    struct _listnode *prev;
 } ListNode;
 
-//Call to create the first node in the linked list
 ListNode headNode(){
     ListNode head;
     head.size = mem_size-1;
@@ -34,21 +33,40 @@ ListNode newNode(ListNode *current, int bytes, int mem){
     ListNode new;
     new.size = mem - bytes - 1;
     new.free = 0;
-    new.location = current->location + bytes;
+    new.location = current->location + bytes + 1;
     new.next = NULL;
     new.prev = current;
     //Updates current node's parameters
-    current->free = 1;
     current->size = bytes;
+    current->free = 1;
     current->next = &new;
     return new;
 }
-}
+
+
 //malloc
 //I think for malloc if it is uninitialized start with a struct that occupies the given bytes from memory[0] and return a pointer to that and make a new (next) struct for the rest of the memory and set that to free.
 //Then on subsequent calls you would go through the linked list until you find one that is free and large enough then you would cut that based on how many bytes are needed and create a new linked list with the remaining unused bytes which would still be free.
 void *mymalloc(size_t size, char *file, int line){
-
+    //If 1, then this is not the first run
+    static int firstRun = 0;
+    static ListNode ptr;
+    //Remaining memory
+    static int mem = mem_size - 1;
+    //Start linked list if it is the first run
+    if (firstRun == 0){
+        ptr = headNode();
+        firstRun++;
+    }
+    else{
+        ptr = *ptr.next;
+    }
+   if (ptr.size >= size){
+       if (ptr.free == 0){
+            ListNode temp = newNode(&ptr, size, mem);
+            mem = mem - size - 1;
+       }
+    }
 }
 
 
