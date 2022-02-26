60c9b3875cf826d9f555d2c002414836a517eb3a
TheRealKlin4 <100099801+TheRealKlin4@users.noreply.github.com>
2022-02-26 15:26:54 -0500

    Update memgrind.c

--- memgrind.c
+++ memgrind.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mymalloc.h"
 
+int randnum(){
+    int num = (rand() % 10);
+    return num;
+}
+int main(int argc, char* argv[]){
+    for (int i=0; i<120; i++){
+        char* ptr = malloc(1);
+        free(ptr);
+    }
+    char* ptrArray[120];
+    for (int i=0; i<120; i++){
+        ptrArray[i] = malloc(1);
+    }
+    for (int i=0; i<120; i++){
+        free(ptrArray[i]);
+    }
+    int a = 0;
+    for(int i=0; i<120; i++){
+        int random = randnum();
+        if(random < 5){
+            ptrArray[a] = malloc(1);
+            a++;
+        }
+        else{
+            if(a == 0){
+                break;
+            }
+            else{
+                a = a-1;
+                free(ptrArray[a]);
+            }
+        }
+    }
+}
