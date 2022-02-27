a447a990a3b8ae4d2104969c963ca816e22b1420
TheRealKlin4 <100099801+TheRealKlin4@users.noreply.github.com>
2022-02-27 15:36:17 -0500

    Update mymalloc.c

--- mymalloc.c
+++ mymalloc.c
@@ -7,115 +7,63 @@
 
 static char memory[mem_size];
 
-//Basic struct can add stuff later
-typedef struct _listnode{
-    // Number of bytes in memory block
-    int size;
-    //0 if free, 1 if used
-    int free;
-    struct _listnode *next;
-} ListNode;
-
-//malloc
-//I think for malloc if it is uninitialized start with a struct that occupies the given bytes from memory[0] and return a pointer to that and make a new (next) struct for the rest of the memory and set that to free.
-//Then on subsequent calls you would go through the linked list until you find one that is free and large enough then you would cut that based on how many bytes are needed and create a new linked list with the remaining unused bytes which would still be free.
 void *mymalloc(size_t size, char *file, int line){
-    //If someone mallocs 0
+    
+    //If malloc 0, return null
     if(size == 0){
         return NULL;
     }
+
     ListNode *ptr;
     ptr = (ListNode*)memory;
-    //Start linked list if this is the first malloc
+    size += sizeof(ptr);
     if (ptr->size == 0){
-        ptr->size = mem_size - 1;
+        ptr->size = mem_size;
         ptr->free = 0;
-        ptr->next = NULL;
     }
     //Iterate through linked list until a free block has been reached
-    while(ptr->free == 1){
-        ptr = ptr->next;
-    }
-    //Check if there is enough free space in this block
-    if (ptr->size >= size){
-        //Only make a new node if the chunk can be fragmented
-        if(ptr->size > size){
+    while(ptr != NULL && ptr->size != 0){
+        if(ptr->size >= size && ptr->free == 0){
             ListNode *new;
-            new = (ListNode*)((char*)ptr+size);
-            new->size = ptr->size - size - 1;
+
+            new = (ListNode*)((char*)ptr+size);    
+            new->size = ptr->size - size;
             new->free = 0;
-            //If ptr is the latest chunk
-            if(ptr->next == NULL){
-                new->next = NULL;
-            }
-            //If this chunk isn't the last
-            else{
-                new->next = ptr->next;
-            }
-            ptr->next = new;        
+
             ptr->size = size;
+            ptr->free = 1;
+            return (char*)ptr+1;
         }
-        //Update ptr parameters
-        ptr->free = 1;
+        ptr = (ListNode*)((char*)ptr + ptr->size);
     }
-    //There is not enough space in this chunk
-    else{
-        //Continue iterating through list to find a large enough free chunk
-        while(ptr!=NULL){
-            ptr = ptr->next;
-            //Return null if end of the list is reached with no free chunk found
-            if(ptr == NULL){
-                printf("Error: not enough memory\n");
-                return NULL;
-            }
-            //If a large enough chunk that is free is found, allocate it
-            if(ptr->size >= size && ptr->free == 0){
-                if(ptr->size > size){
-                    ListNode *new;
-                    new = (ListNode*)((char*)ptr+size);
-                    new->size = ptr->size - size - 1;
-                    new->free = 0;
-                    //If ptr is the latest chunk
-                    if(ptr->next == NULL){
-                        new->next = NULL;
-                    }
-                    //If this chunk isn't the last
-                    else{
-                        new->next = ptr->next;
-                    }
-                    ptr->next = new;        
-                    ptr->size = size;
-                }
-                //Update ptr parameters
-                ptr->free = 1;
-                break;
-            }
-        }
-    }
-    return (char*)ptr+1;
+    printf("ERROR: not enough memory\n");
+    return NULL;
+            
 }
-
-
-
-//free
 void myfree(void *ptr, char *file, int line){
-    ListNode* pointer = (ListNode*)memory;
-    //Checking if the memory has not been malloced to
-    if (pointer->size == 0){
-        printf("ERROR, Memory is uninitialized");
+    //Checking if the ptr is not in the memory array at all
+    if((char*)ptr < memory || (char*)ptr > memory + mem_size){
+        printf("ERROR: not in memory array\n");
         return;
     }
+    ListNode* pointer = (ListNode*)memory;
+    if (pointer->size == 0){
+        printf("ERROR: memory array is uninitialized\n");
+        return;
+    }
+
     //Callling free with an address not obtained from malloc() or not at the start of a chunk
     int checker = 0;
-    while(pointer!=NULL){
-        if((char*)pointer == (char*)ptr-1){
+    
+    while(pointer!=NULL && pointer->size!=0){
+        if(pointer == (ListNode*)((char*)ptr - 1)){
             checker = 1;
             break;
         }
-        pointer = pointer->next;
+        pointer = (ListNode*)((char*)pointer + pointer->size);
     }
     if(checker == 0){
-        printf("Given pointer input is not valid\n");
+        printf("ERROR: given pointer is not at the start of a chunk\n");
         return;
     }
     else{
@@ -125,32 +73,23 @@ void myfree(void *ptr, char *file, int line){
         }
         //If the chunk is already freed, return error
         else{
-            printf("This chunk has already been freed\n");
+            printf("ERROR: this chunk has already been freed\n");
             return;
         }
+      
     }
     //Coalesce empty adjacent memory chunks
     pointer = (ListNode*)memory;
-    while(pointer->next != NULL){ 
-        if(pointer->free == 0 && pointer->next->free == 0){
-            pointer->size = pointer->size + pointer->next->size + 1;
-            pointer->next = pointer->next->next;
+    ListNode* next = (ListNode*)((char*)pointer + pointer->size);
+    while(next!= NULL && next->size!=0){ 
+        next = (ListNode*)((char*)pointer + pointer->size);
+        if(pointer->free == 0 && next->free == 0){
+            pointer->size = pointer->size + next->size;
             pointer = (ListNode*)memory;
         }
         else{
-            pointer = pointer->next;
+            pointer = next;
         }
     }
     return;
-}
-//Has to coalesce adjacent byte chunks into one chunk
-//Error:
-//Calling free with an address not obtained from malloc
-//Calling free with an address not at the start of a chunk
-//Calling free a second time on the same pointer
-
-//Optional: Function that can detect memory leaks
-
-int main(int argc, char* argv[]){
-
 }
