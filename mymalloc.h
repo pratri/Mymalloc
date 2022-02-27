f46d5b26cfd74b2c38384ad7331ccdbbf8f113fb
TheRealKlin4 <100099801+TheRealKlin4@users.noreply.github.com>
2022-02-27 15:36:35 -0500

    Update mymalloc.h

--- mymalloc.h
+++ mymalloc.h
@@ -1,10 +1,17 @@
-#ifndef MYMALLOC_H
-#define MYMALLOC_H
+#ifndef _MYMALLOC_H_
+#define _MYMALLOC_H_
 #define malloc(s) mymalloc(s, __FILE__, __LINE__)
 #define free(p) myfree(p, __FILE__, __LINE__)
 
-void *mymalloc(size_t size, char *file, int line);
+typedef struct _listnode{
+    // Number of bytes in memory block
+    int size;
+    //0 if free, 1 if used
+    int free;
+    struct _listnode *next;
+} ListNode;
 
+void *mymalloc(size_t size, char *file, int line);
 void myfree(void *ptr, char *file, int line);
 
 #endif
