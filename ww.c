31a17cd1e2c4115ce7bd20c4c2bc94bb6225595f
TheRealKlin4 <100099801+TheRealKlin4@users.noreply.github.com>
2022-03-27 11:11:37 -0400

    Update ww.c

--- ww.c
+++ ww.c
@@ -2,11 +2,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <dirent.h>
 
 int main(int argc, char* argv[])
 {
     int col;
     int file;
+
+    DIR *dir;
+    int isDir = 0;
+
     if(argc > 2){
         col = atoi(argv[1]);
         file = open(argv[2], O_RDWR);
@@ -15,65 +20,78 @@ int main(int argc, char* argv[])
         perror("ERROR");
         exit(EXIT_FAILURE);
     }
+    //If the second input is not a file, check if it is a directory
     if(file == -1){
-        perror("ERROR");
-        exit(EXIT_FAILURE);
+        dir = opendir(argv[2]);
+        //If the second input is not a directory, return error
+        if(dir == NULL){
+            perror("ERROR");
+            exit(EXIT_FAILURE);
+        }
+        isDir = 1;
     }
-	char* buffer = malloc(1000);
-	read(file, buffer, 1000);
-    close(file);
-    file = open(argv[2], O_WRONLY);	
+    //If the input is a file, not a directory
+    if(isDir == 0){
+        char* buffer = malloc(1000);
+	    read(file, buffer, 1000);
+        close(file);
+        file = open(argv[2], O_WRONLY);	
 
-	int pos = 1;
-	int i = 0;
-	int wordLength = 0;
-	int exceedLim = 0;
+	    int pos = 1;
+	    int i = 0;
+	    int wordLength = 0;
+	    int exceedLim = 0;
 
-	while(buffer[i]!='\0'){
-		//If the next character is a white space
-		if(buffer[i]==' '){
-            //If the white space is not going to be the first character in a line, add it
-            if(pos!=1 && pos<col){
+	    while(buffer[i]!='\0'){
+		    //If the next character is a white space
+		    if(buffer[i]==' '){
+                //If the white space is not going to be the first character in a line, add it
+                if(pos!=1 && pos<=col){
+                    dprintf(file, "%c", buffer[i]);
+                    pos++;
+                }
+                //Iterate through all the remaining white spaces
+                while(buffer[i]==' '){
+                    i++;
+                }
+		    }
+		    int ptr = i;
+		    //Find the length of the word
+		    while(buffer[ptr]!=' '){
+                if(buffer[ptr]=='\0' || buffer[ptr]=='\n' || buffer[ptr]=='\t'){
+                    break;
+                }
+			    ptr++;
+			    wordLength++;
+		    }
+		    //If the length of the word is longer than the remaining space in the line it isn't the start of a new line, start a new line
+		    if(pos!=1 && wordLength>(col-pos)){
+			    dprintf(file, "%c", '\n');
+			    pos = 1;
+                exceedLim = 1;
+		    }
+            //Write the word into the file
+		    while(i<ptr){
+			    dprintf(file, "%c", buffer[i]);
+			    pos++;
+			    i++;
+                wordLength = 0;
+		    }
+            //If the position after printing out the word hasn't reached the col limit, add the next character, which should be the white space or other kind of non character character
+            if(pos<=col){
                 dprintf(file, "%c", buffer[i]);
                 pos++;
-            }
-            //Iterate through all the remaining white spaces
-            while(buffer[i]==' '){
                 i++;
             }
-		}
-		int ptr = i;
-		//Find the length of the word
-		while(buffer[ptr]!=' '){
-            if(buffer[ptr]=='\0' || buffer[ptr]=='\n' || buffer[ptr]=='\t'){
-                break;
-            }
-			ptr++;
-			wordLength++;
-		}
-		//If the length of the word is longer than the remaining space in the line it isn't the start of a new line, start a new line
-		if(pos!=1 && wordLength>(col-pos)){
-			dprintf(file, "%c", '\n');
-			pos = 1;
-            exceedLim = 1;
-		}
-        //Write the word into the file
-		while(i<ptr){
-			dprintf(file, "%c", buffer[i]);
-			pos++;
-			i++;
-            wordLength = 0;
-		}
-        //If the position after printing out the word hasn't reached the col limit, add the next character, which should be the white space or other kind of non character character
-        if(pos<col){
-            dprintf(file, "%c", buffer[i]);
-            pos++;
-            i++;
+	    }
+	    free(buffer);
+	    close(file);
+        if(exceedLim!=0){
+            exit(EXIT_FAILURE);
         }
-	}
-	free(buffer);
-	close(file);
-    if(exceedLim!=0){
-        exit(EXIT_FAILURE);
+    }
+    //If the input is a directory
+    else{
+
     }
 }
